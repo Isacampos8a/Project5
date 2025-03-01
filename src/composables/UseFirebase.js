8bdf9c9a6f4404c71e4d39b093ff02b79f28bc93
Isabel Campos <isacampos8a@gmail.com>
2025-03-01 15:11:24 -0600

    Installed Firebase

---
+++ src/composables/UseFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa-OyfbMSQDc1vLQUkBldY9lOd7sOJrGw",
+  authDomain: "time-cards-d984a.firebaseapp.com",
+  projectId: "time-cards-d984a",
+  storageBucket: "time-cards-d984a.firebasestorage.app",
+  messagingSenderId: "500614236156",
+  appId: "1:500614236156:web:4efaec1aa193f1731e9479"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
