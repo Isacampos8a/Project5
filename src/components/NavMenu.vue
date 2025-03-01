2d7a30ed38651d7532fefbea510b7e5211f990d2
Isabel Campos <isacampos8a@gmail.com>
2025-03-01 14:35:49 -0600

    Finished Nav Menu

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,6 +2,9 @@
   <nav class="bg-yellow-200 p-5">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/other">Other</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+    <RouterLink to="/Settings">Settings</RouterLink>
+
   </nav>
 </template>
 
