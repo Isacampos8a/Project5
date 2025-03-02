47faae12c07967244ca2e80d87c680112dfcc3c1
Isabel Campos <isacampos8a@gmail.com>
2025-03-02 15:15:05 -0600

    Pagination working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,12 @@
 <script setup>
   import { ref } from 'vue'
   import MainCards from '@/components/MainCards.vue';
-
+  import MainPagination from '@/components/MainPagination.vue';
 
 </script>
 
 <template>
   <MainCards class="bg-yellow-50"></MainCards>
+  <MainPagination></MainPagination>
   
 </template>
