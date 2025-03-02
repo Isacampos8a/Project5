81f712d2baf47e0ba43c1fe7304c7f1bee10caa5
Isabel Campos <isacampos8a@gmail.com>
2025-03-02 15:58:07 -0600

    Pagination styling finished

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -4,7 +4,7 @@ const pages = ref(10)
 const activePage = ref(2)
 
 const prevPage = () => {
-    if (activePage > 1) {
+    if (activePage.value > 1) {
         activePage.value--
     }
     console.log(activePage.value)
@@ -23,9 +23,9 @@ const nextPage = () => {
 
 </script>
 <template>
-    <div>
-        <button @click="prevPage" :disabled="activePage === 1">PREV</button>
-        <button v-for="page in pages" :key="page" class="" >{{ page }}</button>               
-        <button @click="nextPage" :disabled="activePage === pages">NEXT</button>
+    <div class="flex justify-center gap-4 p-8 bg-yellow-50">
+        <button @click="prevPage" :disabled="activePage === 1" class="rounded-lg bg-slate-100 p-2 font-medium text-slate-800 shadow-md">PREV</button>
+        <button v-for="page in pages" :key="page" class="rounded-lg bg-slate-100 p-2 font-medium text-slate-800 shadow-md" :class="page === activePage ? 'bg-yellow-300 text-slate-700' : '' " >{{ page }}</button>               
+        <button @click="nextPage" :disabled="activePage === pages" class="rounded-lg bg-slate-100 p-2 font-medium text-slate-800 shadow-md">NEXT</button>
     </div>
 </template>
