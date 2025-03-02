0244034502288662e982e3d19e294b2afe641eeb
Isabel Campos <isacampos8a@gmail.com>
2025-03-02 14:49:47 -0600

    Updated Nav Guard

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,9 +1,23 @@
+<script setup>
+import { useAuth } from '../composables/useAuth';
+const {isAuthenticated, logout, user } = useAuth()
+</script>
+
 <template >
-  <nav class="bg-yellow-200 p-5">
+  <nav class="bg-yellow-200 p-5 ">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/other">Other</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
     <RouterLink to="/Settings">Settings</RouterLink>
+
+    <div v-if="isAuthenticated">
+      Welcome {{ user.email }}
+      <button @click="logout" class="bg-red-500 px-4 py-2 mx-2 text-white hover:bg-red-800">LOGOUT</button>
+    </div>
+    <div v-else>
+      <RouterLink to="/login">Login</RouterLink>
+
+    </div>
+
 
   </nav>
 </template>
